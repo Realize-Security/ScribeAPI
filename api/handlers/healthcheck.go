77fdd55d3cc8bbfb8b2476f65a6acb47617aa8ad
Richard Curteis <richard@realizesec.com>
2024-08-10 17:05:11 -0700

    add healthcheck routes

--- api/handlers/healthcheck.go
+++ api/handlers/healthcheck.go
@@ -1,1 +1,18 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func ApiHealthCheckRoutes(r *gin.RouterGroup) {
+	healthcheck := r.Group("/api")
+	{
+		healthcheck.Any("/", apiHealthCheck)
+		healthcheck.Any("/api/health", apiHealthCheck)
+	}
+}
+
+func apiHealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, nil)
+}
