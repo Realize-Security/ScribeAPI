2ea004aecf1871567667e96c34fac591b9c44980
Richard Curteis <richard@realizesec.com>
2025-07-23 20:41:42 +0100

    Refactor LogUserFindByIDFailed

--- api/pkg/config/users.go
+++ api/pkg/config/users.go
@@ -14,7 +14,7 @@ const (
 	LogUserDeleteFailed            = "failed to delete user: %s"
 	LogUserFindByEmailFailed       = "failed to find user with email: %s"
 	LogUserFindByEmailSuccess      = "user found with email: %s"
-	LogUserFindByIDFailed          = "failed to find user with UserID: %s"
+	LogUserFindByIDFailed          = "failed to find user with ID: %d"
 	LogUserFindByIDSuccess         = "user found with UserID: %s"
 	LogUserGetFromContextFailed    = "failed to get user from context"
 )
