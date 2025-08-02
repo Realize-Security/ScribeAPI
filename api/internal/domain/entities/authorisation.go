722dd29dc820998d15caef17128e1111e5a09163
Richard Curteis <richard@realizesec.com>
2025-08-02 18:52:48 +0100

    Remove description field from PermissionDBModel and set Permissions []*PermissionDBModel

--- api/internal/domain/entities/authorisation.go
+++ api/internal/domain/entities/authorisation.go
@@ -2,7 +2,13 @@ package entities
 
 type RoleDBModel struct {
 	Base
-	RoleName    string         `gorm:"column:role_name;type:varchar(255);unique;not null"  json:"name" binding:"required"`
-	Description string         `gorm:"column:description;type:varchar(255);not null"  json:"description" binding:"required"`
-	Users       []*UserDBModel `gorm:"many2many:user_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	RoleName    string               `gorm:"column:role_name;type:varchar(255);unique;not null"`
+	Description string               `gorm:"column:description;type:varchar(255);not null"`
+	Users       []*UserDBModel       `gorm:"many2many:user_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Permissions []*PermissionDBModel `gorm:"many2many:permission_name;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+type PermissionDBModel struct {
+	Base
+	PermissionName string `gorm:"column:permission_name;type:varchar(255);unique;default:not null"`
 }
