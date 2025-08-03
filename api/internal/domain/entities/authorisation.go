3fb338121344cc935f0a22fa43a7835316a01e56
Richard Curteis <richard@realizesec.com>
2025-08-03 20:05:03 +0100

    Update role/permission associations

--- api/internal/domain/entities/authorisation.go
+++ api/internal/domain/entities/authorisation.go
@@ -5,10 +5,11 @@ type RoleDBModel struct {
 	RoleName    string               `gorm:"column:role_name;type:varchar(255);unique;not null"`
 	Description string               `gorm:"column:description;type:varchar(255);not null"`
 	Users       []*UserDBModel       `gorm:"many2many:user_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Permissions []*PermissionDBModel `gorm:"many2many:permission_name;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Permissions []*PermissionDBModel `gorm:"many2many:role_permissions;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type PermissionDBModel struct {
 	Base
-	PermissionName string `gorm:"column:permission_name;type:varchar(255);unique;default:not null"`
+	PermissionName string         `gorm:"column:permission_name;type:varchar(255);unique;not null"`
+	Roles          []*RoleDBModel `gorm:"many2many:role_permissions;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
