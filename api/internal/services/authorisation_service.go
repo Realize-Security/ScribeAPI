cdb7f7c8be65fe41b20db8595a92f755901cadbd
Richard Curteis <richard@realizesec.com>
2025-08-03 22:04:59 +0100

    Add permission validation handlers

---
+++ api/internal/services/authorisation_service.go
@@ -0,0 +1,118 @@
+package services
+
+import (
+	"Scribe/internal/domain/entities"
+	"Scribe/internal/domain/repositories"
+	"Scribe/internal/infrastructure/cache"
+	"Scribe/internal/infrastructure/database"
+	"Scribe/pkg/config"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+type AuthorisationRepository interface {
+	CreatNewUser() gin.HandlerFunc
+}
+
+type AuthorisationService struct {
+	ur              repositories.UserRepository
+	sessionCache    *cache.Cache[int, entities.SessionState]
+	permissionCache *cache.Cache[string, int]
+}
+
+func NewAuthorisationService(ur repositories.UserRepository) (*AuthorisationService, error) {
+	return &AuthorisationService{
+		ur:              ur,
+		sessionCache:    cache.SessionCache.Get(),
+		permissionCache: cache.PermissionIDCache.Get(),
+	}, nil
+}
+
+// userHasPermission verifies user has all permissions for user creation
+func (auth *AuthorisationService) userHasPermission(c *gin.Context, requester *entities.UserDBModel, neededPermissions []string) {
+	permissionCache := cache.PermissionIDCache.Get()
+	if permissionCache.Len() == 0 {
+		err := auth.CachePermissionIDs()
+		if err != nil {
+			log.Print("unable to cache permissions")
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	userSession, ok := auth.sessionCache.Get(requester.ID)
+	if !ok {
+		c.AbortWithStatus(http.StatusForbidden)
+		return
+	}
+
+	needed, err := auth.GetIDsForPermissionStrings(neededPermissions)
+	if err != nil {
+		log.Print("unable to map permission names and IDs")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	userPermMap := make(map[int]bool, len(userSession.PermissionIDs))
+	for _, permID := range userSession.PermissionIDs {
+		userPermMap[permID] = true
+	}
+
+	for _, neededPerm := range needed {
+		if !userPermMap[neededPerm] {
+			auth.LogFailedAuthorisation(requester, needed, neededPerm)
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+	}
+	return
+}
+
+func (auth *AuthorisationService) LogFailedAuthorisation(requester *entities.UserDBModel, needed map[string]int, failedID int) {
+	for key, value := range needed {
+		if value == failedID {
+			log.Printf(config.LogUserUnauthorised, requester.ID, key)
+			return
+		}
+	}
+	return
+}
+
+// CreatNewUser verifies user has all permissions for user creation
+func (auth *AuthorisationService) CreatNewUser(requester *entities.UserDBModel) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		neededPermissions := []string{config.UserList, config.UserCreate, config.UserRead}
+		auth.userHasPermission(c, requester, neededPermissions)
+	}
+}
+
+func (auth *AuthorisationService) CachePermissionIDs() error {
+	ar := repositories.NewAuthorisationRepository(database.Db)
+	err := ar.CachePermissionIDs()
+	if err != nil {
+		log.Printf("error caching permissions: %v", err)
+		return err
+	}
+	return nil
+}
+
+func (auth *AuthorisationService) GetIDsForPermissionStrings(names []string) (map[string]int, error) {
+	var needed map[string]int
+	permissionCache := cache.PermissionIDCache.Get()
+	if permissionCache.Len() == 0 {
+		err := auth.CachePermissionIDs()
+		if err != nil {
+			log.Printf(config.LonUnableToCachePermissions, err)
+			return nil, err
+		}
+	}
+
+	for _, perm := range names {
+		id, ok := permissionCache.Get(perm)
+		if ok {
+			needed[perm] = id
+		}
+	}
+	return needed, nil
+}
