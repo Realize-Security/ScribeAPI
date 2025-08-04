71446127382b28544d8afdfbd3709654bd646eef
Richard Curteis <richard@realizesec.com>
2025-08-04 20:05:50 +0100

    remove unused config constants

--- api/pkg/config/app.go
+++ api/pkg/config/app.go
@@ -1,11 +1,8 @@
 package config
 
 const (
-	ApiName                  = "ScribeAPI"
-	CookieDomain             = "realizesec.com"
-	Logfile                  = "/vol/log/application.log"
-	ErrorLog                 = "/vol/log/error.log"
-	LogLogfileCreationFailed = "Creating of standard logging file failed"
-	ApiMessage               = "message"
-	ApiError                 = "error"
+	ApiName      = "ScribeAPI"
+	CookieDomain = "realizesec.com"
+	ApiMessage   = "message"
+	ApiError     = "error"
 )
