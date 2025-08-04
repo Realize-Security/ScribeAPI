6fcf6a35df9540ac4bda2416f0cca6aa5ea96605
Richard Curteis <richard@realizesec.com>
2025-08-04 20:05:03 +0100

    remove unused config constants

--- api/pkg/config/app.go
+++ api/pkg/config/app.go
@@ -3,19 +3,9 @@ package config
 const (
 	ApiName                  = "ScribeAPI"
 	CookieDomain             = "realizesec.com"
-	MasterOrganisation       = "Realize Security"
-	MasterDomain             = "realizesec.com"
-	MasterUserFirstName      = "Janus"
-	MasterUserLastName       = "Admin"
-	MasterUserEmail          = "janus-admin@realizesec.com"
-	GlobalAdminName          = "GLOBAL_ADMIN"
-	MinEntropyBits           = 60
 	Logfile                  = "/vol/log/application.log"
 	ErrorLog                 = "/vol/log/error.log"
 	LogLogfileCreationFailed = "Creating of standard logging file failed"
 	ApiMessage               = "message"
 	ApiError                 = "error"
-	ApiResult                = "result"
-	APIInvalidRequestMessage = "Invalid request"
-	IDRegex                  = `^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`
 )
--- api/pkg/config/database.go
+++ api/pkg/config/database.go
@@ -3,14 +3,9 @@ package config
 import "time"
 
 const (
-	CacheDBConfigSettings      = "DBConfigSettings"
-	DBMaxIdleConnectionsName   = "DBMaxIdleConnections"
-	DBMaxIdleConnectionsValue  = 10
-	DBMaxOpenConnectionsString = "DBMaxOpenConnections"
-	DBMaxOpenConnectionsValue  = 100
-	ConnMaxLifetimeValue       = time.Second * 90
-	DBHealthCheckInterval      = time.Second * 5
-	WhereOrganisationID        = "organisation_id = ?"
-	WhereID                    = "id = ?"
-	DBInitialisationFailed     = "database initialisation failed: %s"
+	DBMaxIdleConnectionsValue = 10
+	DBMaxOpenConnectionsValue = 100
+	ConnMaxLifetimeValue      = time.Second * 90
+	DBHealthCheckInterval     = time.Second * 5
+	DBInitialisationFailed    = "database initialisation failed: %s"
 )
