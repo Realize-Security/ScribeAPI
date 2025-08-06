cdaf25317b605a6554821c13783069f7507a5450
Richard Curteis <richard@realizesec.com>
2025-08-06 19:44:41 +0100

    fixed perm/role db relationships

--- api/internal/domain/entities/authorisation.go
+++ api/internal/domain/entities/authorisation.go
@@ -5,11 +5,19 @@ type RoleDBModel struct {
 	RoleName    string               `gorm:"column:role_name;type:varchar(255);unique;not null"`
 	Description string               `gorm:"column:description;type:varchar(255);not null"`
 	Users       []*UserDBModel       `gorm:"many2many:user_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Permissions []*PermissionDBModel `gorm:"many2many:role_permissions;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Permissions []*PermissionDBModel `gorm:"many2many:role_permissions;joinForeignKey:role_id;joinReferences:permission_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type PermissionDBModel struct {
 	Base
 	PermissionName string         `gorm:"column:permission_name;type:varchar(255);unique;not null"`
-	Roles          []*RoleDBModel `gorm:"many2many:role_permissions;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Roles          []*RoleDBModel `gorm:"many2many:role_permissions;joinForeignKey:permission_id;joinReferences:role_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+func (RoleDBModel) TableName() string {
+	return "roles"
+}
+
+func (PermissionDBModel) TableName() string {
+	return "permissions"
 }
