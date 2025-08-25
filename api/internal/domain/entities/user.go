cb3930d17f190ea3336749dc1f0597447662900f
Richard Curteis <richard@realizesec.com>
2025-08-25 11:25:24 +0100

    added missing UserDBModel columns refs

--- api/internal/domain/entities/user.go
+++ api/internal/domain/entities/user.go
@@ -5,18 +5,20 @@ import (
 )
 
 type UserDBModel struct {
-	ID                 int64          `db:"id"`
-	UUID               string         `db:"uuid"`
-	CreatedAt          time.Time      `db:"created_at"`
-	UpdatedAt          time.Time      `db:"updated_at"`
-	DeletedAt          *time.Time     `db:"deleted_at"`
-	FirstName          string         `db:"first_name" validate:"required"`
-	LastName           string         `db:"last_name" validate:"required"`
-	Email              string         `db:"email" validate:"required,email"`
-	Password           string         `db:"password" validate:"required"`
-	IsActive           bool           `db:"is_active"`
-	TermsAccepted      bool           `db:"terms_accepted" validate:"required"`
-	PasswordResetToken *string        `db:"password_reset_token"`
-	OrganisationID     *string        `db:"organisation_id"`
-	Roles              []*RoleDBModel `db:"-"`
+	ID                     int64          `db:"id"`
+	UUID                   string         `db:"uuid"`
+	CreatedAt              time.Time      `db:"created_at"`
+	UpdatedAt              time.Time      `db:"updated_at"`
+	DeletedAt              *time.Time     `db:"deleted_at"`
+	FirstName              string         `db:"first_name" validate:"required"`
+	LastName               string         `db:"last_name" validate:"required"`
+	Email                  string         `db:"email" validate:"required,email"`
+	Password               string         `db:"password" validate:"required"`
+	IsActive               bool           `db:"is_active"`
+	TermsAccepted          bool           `db:"terms_accepted" validate:"required"`
+	PasswordResetToken     *string        `db:"password_reset_token"`
+	PasswordResetExpiresAt *time.Time     `db:"password_reset_expires_at"`
+	LastLoginAt            *time.Time     `db:"last_login_at"`
+	OrganisationID         *string        `db:"organisation_id"`
+	Roles                  []*RoleDBModel `db:"-"`
 }
