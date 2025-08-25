4dde93381331006e19d2722c3b1a9b833c21f584
Richard Curteis <richard@realizesec.com>
2025-08-25 10:54:11 +0100

    added messages

--- api/pkg/config/users.go
+++ api/pkg/config/users.go
@@ -1,22 +1,24 @@
 package config
 
 const (
-	MinUserLen                     = 1
-	MaxUserLen                     = 30
-	MessageInvalidCredentialsError = "Credentials did not match"
-	MessageUserIsBlocked           = "user blocked from logging in"
-	MessageMissingRequired         = "Missing required parameters."
-	MessageAccountLocked           = "Unable to log in.Please contact your administrator."
-	MessageTermsNotAccepted        = "terms and conditions not accepted"
-	LogUserCreateSuccess           = "User created successfully: %s"
-	LogUserCreateFailed            = "failed to create new user"
-	LogUserUpdateSuccess           = "user updated successfully: %s"
-	LogUserUpdateFailed            = "user update failed: %s"
-	LogUserDeleteSuccess           = "user successfully deleted: %s"
-	LogUserDeleteFailed            = "failed to delete user: %s"
-	LogUserFindByEmailFailed       = "failed to find user with email: %s"
-	LogUserFindByEmailSuccess      = "user found with email: %s"
-	LogUserFindByIDFailed          = "failed to find user with ID: %d"
-	LogUserFindByIDSuccess         = "user found with ID: %d"
-	LogUserGetFromContextFailed    = "failed to get user from context"
+	MinUserLen                       = 1
+	MaxUserLen                       = 30
+	MessageInvalidCredentialsError   = "Credentials did not match"
+	MessageUserIsBlocked             = "user blocked from logging in"
+	MessageMissingRequired           = "Missing required parameters."
+	MessageAccountLocked             = "Unable to log in.Please contact your administrator."
+	MessageTermsNotAccepted          = "terms and conditions not accepted"
+	MessageUserLoggedOutSuccessfully = "Logged out successfully"
+	MessageUserLogOutFailed          = "Log out failed"
+	LogUserCreateSuccess             = "User created successfully: %s"
+	LogUserCreateFailed              = "failed to create new user"
+	LogUserUpdateSuccess             = "user updated successfully: %s"
+	LogUserUpdateFailed              = "user update failed: %s"
+	LogUserDeleteSuccess             = "user successfully deleted: %s"
+	LogUserDeleteFailed              = "failed to delete user: %s"
+	LogUserFindByEmailFailed         = "failed to find user with email: %s"
+	LogUserFindByEmailSuccess        = "user found with email: %s"
+	LogUserFindByIDFailed            = "failed to find user with ID: %d"
+	LogUserFindByIDSuccess           = "user found with ID: %d"
+	LogUserGetFromContextFailed      = "failed to get user from context"
 )
