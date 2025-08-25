e86803ffda6526badd22a669386901bd37085720
Richard Curteis <richard@realizesec.com>
2025-08-25 10:53:00 +0100

    added error messages

--- api/pkg/config/app.go
+++ api/pkg/config/app.go
@@ -1,9 +1,11 @@
 package config
 
 const (
-	ApiName         = "ScribeAPI"
-	CookieDomain    = "realizesec.com"
-	ApiMessage      = "message"
-	ApiError        = "error"
-	ValidationError = "validationErrors"
+	ApiName                 = "ScribeAPI"
+	CookieDomain            = "realizesec.com"
+	ApiMessage              = "message"
+	ApiError                = "error"
+	ValidationError         = "validation errors"
+	MessageInvalidJsonError = "Invalid request format"
+	LogInvalidJsonError     = "failed to bind %s request: %v"
 )
